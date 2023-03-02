59811ecc21f3197a66bdde857a8ba74cc6af9665
Rida Rhnizar <rrhnizar@c1r3p7.1337.ma>
2023-03-02 20:27:54 +0100

    first_commit

--- philo/philo.c
+++ philo/philo.c
@@ -6,15 +6,29 @@
 /*   By: rrhnizar <rrhnizar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 18:47:47 by rrhnizar          #+#    #+#             */
-/*   Updated: 2023/03/02 18:53:31 by rrhnizar         ###   ########.fr       */
+/*   Updated: 2023/03/02 20:27:19 by rrhnizar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
 
-int main(void)
+void	*f1(void *x)
 {
-	printf("just test Makefile\n");
+	printf("juste test thread %d\n", *(int *)x);
+	pthread_exit(0);
+}
+int main()
+{
+	pthread_t thread1;
+	int	*x;
+	x = malloc(sizeof(int) * 4);
+	*x = 9;
+
+	printf("i'am processe parent!!!\n");
+	pthread_create(&thread1, NULL, f1, (void *)x);
+	pthread_join(thread1, NULL);
 	return (0);
 }
