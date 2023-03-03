328fd7fb62603a941b918ebe8006470e8ae669d1
Rida Rhnizar <rrhnizar@c1r3p7.1337.ma>
2023-03-03 15:38:17 +0100

    array_threads

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: rrhnizar <rrhnizar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 18:47:47 by rrhnizar          #+#    #+#             */
-/*   Updated: 2023/03/02 20:27:19 by rrhnizar         ###   ########.fr       */
+/*   Updated: 2023/03/03 15:29:53 by rrhnizar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,32 @@
 
 void	*f1(void *x)
 {
-	printf("juste test thread %d\n", *(int *)x);
+	printf("juste test thread %d\n", (int *)x);
 	pthread_exit(0);
 }
 int main()
 {
-	pthread_t thread1;
-	int	*x;
-	x = malloc(sizeof(int) * 4);
-	*x = 9;
-
+	// pthread_t thread1;
+	pthread_t thread[20];
+	int i;
+	
+	i = 0;
+	// int	*x;
+	// x = malloc(sizeof(int) * 4);
+	// *x = 9;
 	printf("i'am processe parent!!!\n");
-	pthread_create(&thread1, NULL, f1, (void *)x);
-	pthread_join(thread1, NULL);
+	while(i < 20)
+	{
+		pthread_create(&thread[i], NULL, f1, (void *)i);
+		i++;
+	}
+	i = 0;
+	// pthread_create(&thread1, NULL, f1, (void *)x);
+	while(i < 20)
+	{
+		pthread_join(thread[i], NULL);
+		i++;
+	}
+	
 	return (0);
 }
