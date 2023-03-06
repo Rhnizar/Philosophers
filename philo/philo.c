673fa3b60408fba7111543b2626f19161e82ba7c
Rida Rhnizar <rrhnizar@c1r3p7.1337.ma>
2023-03-06 21:04:07 +0100

    limit_thread

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: rrhnizar <rrhnizar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 18:47:47 by rrhnizar          #+#    #+#             */
-/*   Updated: 2023/03/03 15:29:53 by rrhnizar         ###   ########.fr       */
+/*   Updated: 2023/03/05 12:46:41 by rrhnizar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,30 @@
 #include <pthread.h>
 #include <stdlib.h>
 
-void	*f1(void *x)
+void	*eating(void *x)
 {
-	printf("juste test thread %d\n", (int *)x);
+	(void)x;
+	printf("I'am eating !!!\n");
+	pthread_exit(0);
+}
+void	*sleeping(void *x)
+{
+	(void)x;
+	printf("I'am sleeping !!!\n");
+	pthread_exit(0);
+}
+void	*thinking(void	*x)
+{
+	(void)x;
+	printf("I'am thinking !!!!\n");
 	pthread_exit(0);
 }
 int main()
 {
-	// pthread_t thread1;
-	pthread_t thread[20];
-	int i;
-	
-	i = 0;
-	// int	*x;
-	// x = malloc(sizeof(int) * 4);
-	// *x = 9;
-	printf("i'am processe parent!!!\n");
-	while(i < 20)
-	{
-		pthread_create(&thread[i], NULL, f1, (void *)i);
-		i++;
-	}
-	i = 0;
-	// pthread_create(&thread1, NULL, f1, (void *)x);
-	while(i < 20)
-	{
-		pthread_join(thread[i], NULL);
-		i++;
-	}
-	
+	pthread_t	t1;
+	pthread_create(&t1, NULL, eating, NULL);
+	pthread_create(&t1, NULL, sleeping, NULL);
+	pthread_create(&t1, NULL, thinking, NULL);
+	pthread_join(t1, NULL);
 	return (0);
 }
--- test/test.c
+++ test/test.c
@@ -6,23 +6,47 @@
 /*   By: rrhnizar <rrhnizar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 17:22:08 by rrhnizar          #+#    #+#             */
-/*   Updated: 2023/03/04 19:45:57 by rrhnizar         ###   ########.fr       */
+/*   Updated: 2023/03/06 16:23:16 by rrhnizar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
-int main() {
-    char    *str;
-    int     i;
-    
-    i = 0;
-    str = malloc(sizeof(char) * 1);
-    while (i < 10000000)
+#include <pthread.h>
+
+int r = 0;
+void    *f1(void *x)
+{
+    (void)x;
+    char *r;
+    r = malloc(sizeof(char) * 5);
+    if(!r)
     {
-        str[i] = 'a';
+        perror("malloc_thread");
+        return(NULL);
+    }
+    int i = 0;
+    while (i < 5)
+        r[i++] = 'a';
+    printf("%s\n", r);
+    return (NULL);
+}
+
+int main() 
+{
+    pthread_t   *th;
+    th = malloc(sizeof(pthread_t) * 1000000);
+    if(!th)
+    {
+        perror("malloc");
+        return(0);
+    }
+    int i = 0;
+    while (1)
+    {
+        pthread_create(&th[i], NULL, f1, NULL);
+        printf("===>%d\n", i);
         i++;
     }
-    printf("%s\n", str);
     return 0;
 }
